0f37d341449cba6ad54cc157304e150d134d3683
ruasvmv <maksim.suvorov@raiffeisen.ru>
2022-03-05 12:15:04 +0300

    Add config for KMeans

--- src/stages/train_model.py
+++ src/stages/train_model.py
@@ -29,7 +29,7 @@ def train_model(config_path: str) -> None:
     logger.info('Loaded train dataset')
 
     start_time = time.time()
-    model = train(x, estimator_name)
+    model = train(x, estimator_name, config['train']['estimators'][estimator_name])
     fitting_time = time.time() - start_time
 
     models_path = config['train']['model_path']
